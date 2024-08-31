691a41b9174e5a52ca182445426258c461342ea7
sourabh.kumar3312@gmail.com <sourabh.kumar3312@gmail.com>
2024-09-01 02:38:03 +0530

    4.1 - Pure node js code 4.2 - understanding of headers, req, response

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const express = require('express');
+// pure node js code without express
 
-let server = express();
+const http = require('http');
+
+let obj ={'name':'sourabh'}
+
+let server = http.createServer((req,res)=>{
+    console.log("server started");
+    console.log(req.url);
+    
+    res.setHeader('Sourabh','Its a header'); // custom header
+    res.setHeader('Content-Type','application/json'); 
+    res.end(JSON.stringify(obj));
+});
+
 server.listen(8080);
 
-//     "nodemon": "^3.1.4" (Major.Minor.Patch) - allowed for minor change only
-//     "nodemon": "*3.1.4" (Major.Minor.Patch) - allowed for major change
-//     "nodemon": "~3.1.4" (Major.Minor.Patch) - allowed for patch change only
+
+// every time the content-type matters always
+    // text/html - treat as html otherwise normal string
+    // application - treat as json otherwise normal string
+
+// we cannot pass object directly to res.end it will give error. so we need to convert that into string. 
+// but the problem is node cannot understand we are passing json to it not string. so we need to use (content-type application/json)
+    // without (content-type application/json) - treat as string
+    // with (content-type application/json) - treat as json
