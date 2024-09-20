2479cff841c826ab4fe48a32c73cc2f053aba9db
sourabh.mahato <sourabh.mahato@mapmyindia.com>
2024-09-20 18:56:34 +0530

    comment added in last commmit...

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 let server = express();
 let route = require('./router');
 
-server.use('/api', route);
+server.use('/api', route); 
+// http://localhost:3001/api/login
 
 server.listen(3001, () => {
   console.log("server started");
